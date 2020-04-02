f6c54c14afc6691327a6ee4d7980adb13fa52c5a
spinon187 <spinon187@gmail.com>
2020-04-01 19:44:02 -0500

    stock time 2

--- stocktime.js
+++ stocktime.js
@@ -9,3 +9,18 @@ var maxProfit = function(prices) {
   }
   return max;
 };
+
+var maxProfit = function(prices) {
+  let total = 0, low = 0, running = 0;
+  for(i=0; i<prices.length; i++){
+    if(prices[i] < prices[i-1]){
+      if(running > 0) total += running;
+      running = 0;
+      low = i;
+    } else {
+      running = Math.max(running, prices[i]-prices[low])
+    }
+  }
+  if(running > 0) total += running
+  return total;
+};
