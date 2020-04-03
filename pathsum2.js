3a30981cf494025b3e6d19b08e150bd5963ce939
spinon187 <spinon187@gmail.com>
2020-04-02 23:51:13 -0500

    path sum 2

---
+++ pathsum2.js
@@ -0,0 +1,21 @@
+const dfs = (node, t, sum, list, final) => {
+  if(!node) return;
+  sum += node.val;
+  if(!node.left && !node.right){
+    if (sum === t){
+      final.push([...list, node.val]);
+    } ;
+  }
+  if(node.left){
+    final = dfs(node.left, t, sum, [...list, node.val], final)
+  }
+  if(node.right){
+    val = dfs(node.right, t, sum, [...list, node.val], final)
+  }
+  return final;
+}
+
+var pathSum = function(root, sum) {
+  if(!root) return [];
+  return dfs(root, sum, 0, [], []);
+};
