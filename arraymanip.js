709218408899a72f2909d245d5cc5886f9a03c27
spinon187 <spinon187@gmail.com>
2020-04-16 21:21:05 -0500

    fuck array manip

--- arraymanip.js
+++ arraymanip.js
@@ -7,9 +7,23 @@ const bloop = (arr, start, end, val, max) => {
 }
 
 function arrayManipulation(n, queries) {
-  let diff = new Array(n).fill(0), max = 0;
-  for(let i in queries){
+  let diff = [];
+  for(let j = 0; j<=n; j++){
+    if(j>=queries[0][0]-1 && j<queries[0][1]){
+      j.push(queries[0][3])
+    } else {
+      j.push(0)
+    }
+  }
+  for(let i = 1; i<queries.length; i++){
     max = bloop(diff, queries[i][0], queries[i][1], queries[i][2], max)
   }
   return max
 }
+
+// const arrayManipulation = (n, queries) => {
+//   let k = {};
+//   for(let i = 0; i<queries.length; i++){
+
+//   }
+// }
