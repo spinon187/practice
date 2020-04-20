cc06faa2bd81c395fe57cb89d1f4dc54965f770a
spinon187 <spinon187@gmail.com>
2020-04-19 21:59:16 -0500

    i hate hackerrank

---
+++ commonchild.js
@@ -0,0 +1,4 @@
+function commonChild(s1, s2) {
+  let d1 = {}, d2 = {};
+  
+}
---
+++ specialstring.js
@@ -0,0 +1,11 @@
+const odd = (s, i, count) => {
+  let a = i, z = i;
+  while(s[a] === s[z]){
+    
+  }
+}
+
+function substrCount(n, s) {
+
+
+}
