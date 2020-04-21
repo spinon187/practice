497a9806325eecb2dde388e7a704738391706227
spinon187 <spinon187@gmail.com>
2020-04-20 20:33:10 -0500

    i dont know

--- commonchild.js
+++ commonchild.js
@@ -1,15 +1,21 @@
 const commonChild = (s1, s2) => {
   s1 = s1.split(''), s2 = s2.split('');
   let d1 = {}, d2 = {}, k1 = new Set([...s1]), k2 = new Set([...s1]);
-  for(let i in s1){
-    if(k2.has(s1[i])){
-      d1[s1[i]] = d1[s1[i]] ? [...d1[s1[i]], i] : [i]
-    }
-  }
+  // for(let i in s1){
+  //   if(k2.has(s1[i])){
+  //     d1[s1[i]] = d1[s1[i]] ? [...d1[s1[i]], i] : [i]
+  //   }
+  // }
   for(let i in s2){
     if(k1.has(s2[i])){
       d2[s2[i]] = d2[s2[i]] ? [...d2[s2[i]], i] : [i]
     }
   }
-  
+  let max = 0;
+  for(let i in s1){
+    let j = i+1, x;
+    for(j;j<s1.length;j++){
+      
+    }
+  }
 };
