62fbe011a0a957976db1f7a25f53eae9e0f8ffde
spinon187 <spinon187@gmail.com>
2020-06-03 17:34:15 -0500

    shift linked list

---
+++ algoexpert/shiftll.py
@@ -0,0 +1,24 @@
+def shiftLinkedList(head, k):
+  l = 1
+  tail = head
+  while tail.next:
+    tail = tail.next
+    l += 1
+  diff = abs(k) % l
+  if diff == 0:
+    return head
+  if k > 0:
+    diff = l - diff
+  newTail = head
+  for i in range(1, diff):
+    newTail = newTail.next
+  newHead = newTail.next
+  newTail.next = None
+  tail.next = head
+  return newHead
+
+# This is the class of the input linked list.
+class LinkedList:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
