a2c3272e9c050bf69cea5e0763d001cb6828573d
spinon187 <spinon187@gmail.com>
2020-06-10 19:50:40 -0500

    unique emails better

--- uniqueemails.py
+++ uniqueemails.py
@@ -1,16 +1,26 @@
+# def numUniqueEmails(self, emails):
+#     count = 0
+#     uniques = {}
+#     for a in emails:
+#         name, domain = a.split('@')
+#         filtered = []
+#         for i in name:
+#             if i == '+':
+#                 break
+#             elif i != '.':
+#                 filtered.append(i)
+#         final = ''.join(filtered) + '@' + domain
+#         if final not in uniques:
+#             count += 1
+#             uniques[final] = True
+#     return count
+
 def numUniqueEmails(self, emails):
     count = 0
-    uniques = {}
+    uniques = set()
     for a in emails:
         name, domain = a.split('@')
-        filtered = []
-        for i in name:
-            if i == '+':
-                break
-            elif i != '.':
-                filtered.append(i)
-        final = ''.join(filtered) + '@' + domain
-        if final not in uniques:
-            count += 1
-            uniques[final] = True
-    return count
+        name = ''.join(name.split('+')[0].split('.'))
+        full = name + '@' + domain
+        uniques.add(full)
+    return len(uniques)
