f0992ed82a65cf36a0af00b76907f3eda0aaaa5c
spinon187 <spinon187@gmail.com>
2020-06-18 19:06:22 -0500

    minimum pathsum o(n) space

--- minpathsum.py
+++ minpathsum.py
@@ -1,17 +1,37 @@
+# class Solution:
+#     def minPathSum(self, grid):
+#         paths = [[0]*len(grid[0])]*len(grid)
+#         for row in range(len(paths)):
+#             for col in range(len(paths[row])):
+#                 if row == 0:
+#                     if col == 0:
+#                         paths[row][col] = grid[row][col]
+#                     else:
+#                         paths[row][col] = grid[row][col] + paths[row][col-1]
+#                 elif col == 0:
+#                     paths[row][col] = grid[row][col] + paths[row-1][col]
+#                 else:
+#                     val = grid[row][col]
+#                     paths[row][col] = min(paths[row-1][col]+val, paths[row][col-1]+val)
+#         print(paths)
+#         return paths[-1][-1]
+
 class Solution:
     def minPathSum(self, grid):
-        paths = [[0]*len(grid[0])]*len(grid)
-        for row in range(len(paths)):
-            for col in range(len(paths[row])):
+        comparison = [0]*len(grid[0])
+        current = []
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                val = grid[row][col]
                 if row == 0:
                     if col == 0:
-                        paths[row][col] = grid[row][col]
+                        current.append(val)
                     else:
-                        paths[row][col] = grid[row][col] + paths[row][col-1]
+                        current.append(val + current[col-1])
                 elif col == 0:
-                    paths[row][col] = grid[row][col] + paths[row-1][col]
+                    current.append(val + comparison[col])
                 else:
-                    val = grid[row][col]
-                    paths[row][col] = min(paths[row-1][col]+val, paths[row][col-1]+val)
-        print(paths)
-        return paths[-1][-1]
+                    current.append(min(comparison[col]+val, current[col-1]+val))
+            comparison = current[:]
+            current = []
+        return comparison[-1]
