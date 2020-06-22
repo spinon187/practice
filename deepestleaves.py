a4e90b6b07af4da800885e6a7d09e259176ff6d2
spinon187 <spinon187@gmail.com>
2020-06-22 11:42:56 -0500

    better deepest leaves sum

--- deepestleaves.py
+++ deepestleaves.py
@@ -1,16 +1,36 @@
+# class Solution:
+#     def deepestLeavesSum(self, root):
+#         table = {}
+#         maxDepth = self.traverse(root, table, 0, 0)
+#         return sum(table[maxDepth])
+        
+        
+#     def traverse(self, node, table, depth, maxDepth):
+#         if not node:
+#             return maxDepth
+#         maxDepth = max(maxDepth, depth)
+#         if depth not in table:
+#             table[depth] = []
+#         table[depth].append(node.val)
+#         maxDepth = max(self.traverse(node.left, table, depth+1, maxDepth), self.traverse(node.right, table, depth+1, maxDepth))
+#         return maxDepth
+
 class Solution:
     def deepestLeavesSum(self, root):
-        table = {}
-        maxDepth = self.traverse(root, table, 0, 0)
-        return sum(table[maxDepth])
-        
-        
-    def traverse(self, node, table, depth, maxDepth):
-        if not node:
-            return maxDepth
-        maxDepth = max(maxDepth, depth)
-        if depth not in table:
-            table[depth] = []
-        table[depth].append(node.val)
-        maxDepth = max(self.traverse(node.left, table, depth+1, maxDepth), self.traverse(node.right, table, depth+1, maxDepth))
-        return maxDepth
+        if not root:
+            return 0
+        q = [root]
+        while q:
+            next_level = []
+            for node in q:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            if len(next_level):
+                q = next_level
+            else:
+                vals = []
+                for node in q:
+                    vals.append(node.val)
+                return sum(vals)
