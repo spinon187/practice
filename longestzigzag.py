e82335b3fd8120fd9a5340eb5bac92a29fc9b5a1
spinon187 <spinon187@gmail.com>
2020-06-28 19:13:16 -0500

    longest zigzag in binary tree

---
+++ longestzigzag.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestZigZag(self, root):
+        if root is None:
+            return 0
+        
+        def traverse(node, left_sum, right_sum):
+            if node.left:
+                next_left = traverse(node.left, right_sum + 1, 0)
+            else:
+                next_left = right_sum
+            if node.right:
+                next_right = traverse(node.right, 0, left_sum + 1)
+            else:
+                next_right = left_sum
+            return max(next_left, next_right)
+            
+        
+        return traverse(root, 0, 0)
